4cc3550ee6c802533aee43a56ca3bd35f2395a1e
Kenjinh <nohara0312@gmail.com>
2025-05-13 16:17:18 -0300

    Inital Commit

---
+++ lua/plugins/telescope-nvim.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+
+    dependencies = {
+      {
+        -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make") == 1
+        end,
+      },
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+      },
+    },
+    keys = {
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+    },
+    cmd = "Telescope",
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("live_grep_args")
+    end,
+  },
+}
